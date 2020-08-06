259cc19ca6483000ecbcecc3de123a543780ee2d
Jorgeluisk <jorgeluist543@gmail.com>
2020-08-06 16:05:22 -0500

    se agregaron las plantillas a las carpetas de Acta de inicio, Administración de riesgos, Alcance y  Minutas

--- Codigo/Sitio Web/ci_ieca/app/Views/admin/template/Footer.php
+++
@@ -1,8 +0,0 @@
-
-<script src="../template/js/bootstrap.min.js"></script>
-    <script src="../template/js/popper.min.js"></script>
-    <script src="../template/bootstrap.min.js"></script>
-    <script src="../template/js/jquery.min.js"></script>
-    <script src="../template/js/scripts.js"></script>
-</body>
-</html>
